584d71e44ae2484631dd49365f3f3ff3224645c3
Thomas Dohmke <ashtom@github.com>
2022-05-23 16:30:55 -0700

    Update created.js

--- .github/apps/hubble/events/issue_comment/created.js
+++ .github/apps/hubble/events/issue_comment/created.js
@@ -1,5 +1,11 @@
 // .github/apps/hubble/events/issue_comment/created.js
-export default function ({ event }) {
+import { kv } from '@extendohub/storage'
+
+export default async ({event}) => {
   const newWords = event.comment.body.match(/\b\w{5}\b/g)
-  console.log(`Five-letter words:\n${newWords.join('\n')}`)
+  if (!newWords || !newWords.length) return
+  const currentWords = await kv.get('words')
+  const words = new Set(currentWords)
+  newWords.forEach(word => words.add(word.toLowerCase()))
+  await kv.set('words', Array.from(words))
 }
